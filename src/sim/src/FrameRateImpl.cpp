54bb3b732c4989fd2afd486d3b8f621c672b4c50
seani <adam.hayek@gmail.com>
2013-08-05 21:23:35 -0500

    add missing copyright notices.

--- src/sim/src/FrameRateImpl.cpp
+++ src/sim/src/FrameRateImpl.cpp
@@ -1,3 +1,21 @@
+//
+// v1 - Prototype character/item/tradeskill game.
+// Copyright (C) 2013 Adam Hayek (adam.hayek@gmail.com)
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see [http://www.gnu.org/licenses/].
+//
+
 #include "sim\IFrameRate.h"
 
 
@@ -134,9 +152,8 @@ bool FrameRateImpl::FrameStatistics(float timeSeconds, Context &caller)
     float recentFps = totalFrameCount / totalTime;
 
     //display the time.
-    #define ShowLine(setParams) frameDisplay.Set setParams; IFBREAKCONTEXTMSG(screenText->PrintLineTopLeft(frameDisplay, context) == false, "Error displaying frame rate on screen.");
-    ubuffer256 frameDisplay;
-    ShowLine((L"%3.0f FPS (%3.0f)", recentFps, smoothFps));
+    ubuffer256 frameDisplay; frameDisplay.Set(L"%3.0f FPS (%3.0f)", recentFps, smoothFps);
+    IFBREAKCONTEXTMSG(screenText->PrintLineTopLeft(frameDisplay, context) == false, "Error displaying frame rate on screen.");
     
     //success
     return true;
